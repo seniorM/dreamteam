f13c43e840202788bdf2dfd43d408d1f5a36fb94
solembum-php <solembum@i.ua>
2019-09-05 21:20:09 +0300

    add to includes/functions/english/index.php

--- includes/functions/english/index.php
+++ includes/functions/english/index.php
@@ -1,47 +1,84 @@
 <?php
 
 function get_registration() {
-    include_once 'templates' . DIRECTORY_SEPARATOR . 'main.php';
+    include_once 'views' . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . 'main.php';
 }
 
-function getLogPass() {
-    if (!file_exists(USERS_LOG_PASS)) {
-	return false;
+function getRequestUser() {
+    global $errors;
+    $login = $_POST['login'];
+    $pass = $_POST['pass'];
+    $pass_conf = $_POST['pass_conf'];
+    if (!$login && !$pass && !$pass_conf) {
+        $errors[] = 'empty data';
+    }
+    if (strlen($login) < 6) {
+        $errors[] = 'login must have more than 5 symbols';
+    }
+    if ($pass !== $pass_conf) {
+        $errors[] = 'passwords do not match';
+    }
+    if (strlen($pass) < 6) {
+        $errors[] = 'password must have more than 5 symbols';
+    }
+    if (count($errors) != 0) {
+        return false;
+    }
+    $user = array(
+        'login' => $login,
+        'pass' => password_hash($pass, PASSWORD_DEFAULT),
+    );
+    return $user;
+}
+
+function userExists($needed_user) {
+    $users = getUsers();
+    if ($users) {
+        foreach ($users as $user) {
+            if ($needed_user['login'] === $user['login']) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function getUsers() {
+    if (!file_exists(USERS_DATA_FILE)) {
+        return false;
     } else {
-	$content = file_get_contents(USERS_LOG_PASS);
-	if (!$content) {
-	    return false;
-	} else {
-	    $users = json_decode($content, true);
-	    return $users;
-	}
+        $content = file_get_contents(USERS_DATA_FILE);
+        if (!$content) {
+            return false;
+        } else {
+            $users = json_decode($content, true);
+            return $users;
+        }
     }
 }
 
-function saveLogPass($users) {
+function saveUsers($users) {
     $content = json_encode($users);
-    $res = file_put_contents(USERS_LOG_PASS, $content);
+    $res = file_put_contents(USERS_DATA_FILE, $content);
     return (bool) $res;
 }
 
-function post_registration($login, $password) {
-    if (!isset($_POST['login']) && !isset($_POST['password'])) {
-	$message = 'отсутствуют входящие данные';
+function addUser($user) {
+    $users = getUsers();
+    $users[] = $user;
+    saveUsers($users);
+}
+
+function post_registration() {
+    $errors = array();
+    
+    $user = getRequestUser();
+
+    if (userExists($user)) {
+        $errors[] = 'user already exists';
+        var_dump($errors);
     } else {
-	$login = $_POST['login'];
-	$password = $_POST['password'];
-	if (empty($login) && empty($password)) {
-	    $message = 'отсутствуют логин и пароль!';
-	} else if (empty($login) && (!empty($password))) {
-	    $message = 'отсутствует логин!';
-	} else if ((!empty($login)) && empty($password)) {
-	    $message = 'отсутствует пароль!';
-	} else {
-	    $users = getLogPass();
-	    $users[] = array(array('login' => $login, 'password' => $password),);
-	    saveLogPass($users);
-	    header('Location:login.php');
-	    
-	}
+        addUser($user);
+        header('Location:login.php');
     }
 }
