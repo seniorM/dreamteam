a0a67ddcf9455ba73ad346cc90e2cc7a9165f6db
seniorM <54437294+seniorM@users.noreply.github.com>
2019-09-06 13:31:44 +0300

    Update main.php

--- views/templates/main.php
+++ views/templates/main.php
@@ -1,10 +1,48 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-    <head>
-	<title>Главная</title>
-    </head>
-    <body>	    
-	          
-        
-    </body>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<meta name="description" content="description"/>
+<meta name="author" content="author"/> 
+<link rel="stylesheet" type="text/css" href="default.css" media="screen"/>
+<link href="../../css/default.css" rel="stylesheet" type="text/css"/>
+<title>Dream team blog</title>
+</head>
+<body>
+<div class="container">
+	<div class="main">
+		<div class="header">
+			<div class="title">
+				<h1>Dream team blog</h1>
+			</div>
+		</div>
+		<div class="content"><div class="inner_copy"></div>
+			<div class="item">
+				<h1>Заголовок новости</h1>
+				<div class="descr">Дата новости. Автор</div>
+				<p>In hac habitasse platea dictumst. Duis porttitor. Sed vulputate elementum nisl. Vivamus et mi at arcu mattis iaculis. Nullam posuere tristique tortor. In bibendum. Aenean ornare, nunc eget pretium porttitor, sem est pretium leo, non euismod nulla dui non diam. Pellentesque dictum faucibus leo. Vestibulum ac ante. Sed in est.
+				Sed sodales nisl sit amet augue. Donec ultrices, augue ullamcorper posuere laoreet, turpis massa tristique justo, sed egestas metus magna sed purus.
+				Aliquam risus justo, mollis in, laoreet a, consectetuer nec, risus. Nunc blandit sodales lacus. Nam luctus semper mi. In eu diam.</p>
+				</p>
+			</div>						
+		</div>
+		<div class="sidenav">
+			<h2>Меню</h2>
+			<ul>
+				<li><a href="/.php">Главная</a></li>
+				<li><a href="/views/pages/login.php">Войти</a></li>				
+			</ul>
+						
+			
+		</div>
+		<div class="clearer"></div>
+	</div>
+	<div class="footer">
+		<span class="left"><a href="#">Dream team blog</a></span>
+		
+		<center>Сайт создан компанией Dream Team &copy; <?php echo date('Y'); ?></center>
+		<div class="clearer"></div>
+	</div>
+</div>
+</body>
 </html>
