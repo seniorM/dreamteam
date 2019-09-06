5a04749706ebad20df824946534e3bf784974e25
Vasyl <admin@gmail.com>
2019-09-06 19:03:57 +0300

    start session

--- includes/functions/georgia/index.php
+++ includes/functions/georgia/index.php
@@ -1,4 +1,23 @@
 <?php
+session_start();
+//получение данных имени если есть
+if (isset($_POST['name'])){
+    $name = $_POST['name'];
+if (empty($name)) {
+    $res = 'Отсутсвие данные';
+} else if (iconv_strlen($name) < 4) {
+    $res = 'Введенно меньше 4 символов';
+} else {
+    $_SESSION['name']=$name;
+}
+if (!empty($res)){
+    echo $res;
+    exit();
+}
+}
+if(empty($_SESSION['name'])){
+    header('Location:index.php');
+} 
 
 
 
