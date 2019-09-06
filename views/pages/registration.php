a1f2c869a13acd7b6e4d46a085b954caaff6ec6b
Vasyl <admin@gmail.com>
2019-09-06 18:23:56 +0300

    changes to views/pages/registration.php

---
+++ views/pages/registration.php
@@ -0,0 +1,12 @@
+<form action="index.php?action=registration" method="post" id="reg">
+    <label>Login:
+	<input type="text" name="login"/>
+    </label>
+    <label>Password:
+	<input type="password" name="pass"/>
+    </label>
+    <label>Confirm password:
+	<input type="password" name="pass_conf"/>
+    </label>
+    <input type="submit" value="Register"/>
+</form>
