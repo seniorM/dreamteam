a240ea216ebf493af014cc7378672c974c94f279
seniorM <54437294+seniorM@users.noreply.github.com>
2019-09-06 13:37:24 +0300

    Update all.php

---
+++ views/pages/all.php
@@ -0,0 +1,22 @@
+<?php
+$posts = get_posts();
+
+?>
+
+<div id="all">
+    <form action="index.php?action=add" method="post" name="add">
+        <label>Heading: <input type="text" name="heading"></label>
+        <label>Text<textarea name="text"></textarea></label>
+        <input type="submit" value="add post">
+    </form>
+    <?php foreach ($posts as $id => $post): ?>
+    <div class="<?= $posts['login'];?>">
+        <h2><?= $posts['heading'];?></h2>
+        <?= $posts['text'];?>
+        <form action="index.php?action=delete" method="post" name="delete">
+            <input type="text" value="<?= $id;?>" hidden>
+            <input type="submit" value="delete">
+        </form>
+    </div>
+    <?php endforeach;?>
+</div>
