d7228bddff0ec30ad729c7ee1d4d74736e6a8ce1
solembum-php <solembum@i.ua>
2019-09-08 14:36:15 +0300

    add to includes/functions/english/index.php and includes/views/pages/index.php

--- includes/functions/english/index.php
+++ includes/functions/english/index.php
@@ -11,12 +11,12 @@ function post_registration() {
     $errors = array();
     $user = get_request_user();
     if (check_login($user['login'])) {
-	$errors[] = 'user already exists';
-	set_errors($errors);
-	header('Location:'. url('registration'));
+        $errors[] = 'user already exists';
+        set_errors($errors);
+        header('Location:' . url('registration'));
     } else {
-	add_user($user);
-	header('Location:'. url('login'));
+        add_user($user);
+        header('Location:' . url('login'));
     }
 }
 
@@ -26,24 +26,24 @@ function get_request_user() {
     $pass = $_POST['pass'];
     $pass_conf = $_POST['pass_conf'];
     if (!$login && !$pass && !$pass_conf) {
-	$errors[] = 'empty data';
+        $errors[] = 'empty data';
     }
     if (strlen($login) < 6) {
-	$errors[] = 'login must have more than 5 symbols';
+        $errors[] = 'login must have more than 5 symbols';
     }
     if ($pass !== $pass_conf) {
-	$errors[] = 'passwords do not match';
+        $errors[] = 'passwords do not match';
     }
     if (strlen($pass) < 6) {
-	$errors[] = 'password must have more than 5 symbols';
+        $errors[] = 'password must have more than 5 symbols';
     }
     if (count($errors) != 0) {
-	set_errors($errors);
-	header('Location:'. url('registration'));
+        set_errors($errors);
+        header('Location:' . url('registration'));
     }
     $user = array(
-	'login' => $login,
-	'pass' => password_hash($pass, PASSWORD_DEFAULT),
+        'login' => $login,
+        'pass' => password_hash($pass, PASSWORD_DEFAULT),
     );
     return $user;
 }
@@ -51,33 +51,33 @@ function get_request_user() {
 function check_login($login) {
     $users = get_users();
     if (!empty($users)) {
-	foreach ($users as $user) {
-	    if ($login === $user['login']) {
-		return true;
-	    }
-	}
+        foreach ($users as $user) {
+            if ($login === $user['login']) {
+                return true;
+            }
+        }
     }
     return false;
 }
 
 function get_users() {
     if (!file_exists(USERS_DATA_FILE)) {
-	return false;
+        return false;
     } else {
-	$content = file_get_contents(USERS_DATA_FILE);
-	if (!$content) {
-	    return $users = [];
-	} else {
-	    $users = json_decode($content, true);
-	    return $users;
-	}
+        $content = file_get_contents(USERS_DATA_FILE);
+        if (!$content) {
+            return $users = [];
+        } else {
+            $users = json_decode($content, true);
+            return $users;
+        }
     }
 }
 
 function save_users($users) {
     $content = json_encode($users);
-    if(!file_exists('data')){
-	mkdir('data');
+    if (!file_exists('data')) {
+        mkdir('data');
     }
     $res = file_put_contents(USERS_DATA_FILE, $content);
     return (bool) $res;
@@ -89,41 +89,21 @@ function add_user($user) {
     save_users($users);
 }
 
-
-function set_errors($errors){   
+function set_errors($errors) {
     $_SESSION['errors'] = $errors;
 }
 
-function get_errors(){
+function get_errors() {
     $errors = $_SESSION['errors'];
     unset($_SESSION['errors']);
     return $errors;
 }
 
-function get_index(){
+function get_index() {
     $login = $_POST['login'];
-    if($login === $_SESSION['login']){
-        foreach ($array as $value) {
-            //all posts
-        }
-    }else{
-        header('Location:'. url('login'));
+    if ($login !== $_SESSION['login']) {
+        header('Location:' . url('login'));
+    } else {
+        $posts = get_posts();
     }
 }
-//function post_login() {
-//    global $errors;
-//    
-//    $pass = $_POST['pass'];
-//    if (!$login && !$pass) {
-//        $errors[] = 'empty data';
-//    } else {
-//        $users = getUsers();
-//        if ($users) {
-//            foreach ($users as $user) {
-//                if ($login === $user['login'] || $pass === $user['pass'])
-//                    $_SESSION['login'] = $login;;
-//            }
-//        }
-//        return false;
-//    }
-//}
--- includes/views/pages/index.php
+++ includes/views/pages/index.php
@@ -1,1 +1,12 @@
-<a href="<?= url('all')?>">Мои записи</a>
+<a href="<?= url('all') ?>">Мои записи</a>
+<?php
+get_index();
+?>
+<div id="index">
+    <?php foreach ($posts as $id => $post): ?>
+        <div class="<?= $post['login']; ?>">
+            <h2><?= $post['heading']; ?></h2>
+            <?= $post['text']; ?>
+        </div>
+    <?php endforeach; ?>
+</div>
