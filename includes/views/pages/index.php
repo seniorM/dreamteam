5d3d91d243e86b4c83603adfcd000310e0f6de94
totoseriapovlup <totoseriapov@yandex.ua>
2019-09-09 19:18:47 +0300

    fix tag

--- includes/views/pages/index.php
+++ includes/views/pages/index.php
@@ -4,7 +4,7 @@
     <?php foreach ($posts as $id => $post) : ?>
     <div class="post">
 	<h2><?= $post['heading'] ?></h2>
-	<div>$post['text']</div>
+	<div><?= $post['text']?></div>
     </div>
     <?php endforeach;?>
 </div>
