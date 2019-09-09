316fd6083f84cdc602f31946360ed5648526cd8a
user <Global@mail.ua>
2019-09-09 19:54:47 +0300

    change main.php

--- includes/views/templates/main.php
+++ includes/views/templates/main.php
@@ -4,8 +4,9 @@
 	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 	<meta name="description" content="description"/>
 	<meta name="author" content="author"/> 
-	<link rel="stylesheet" type="text/css" href="default.css" media="screen"/>
+	
 	<link href="css/default.css" rel="stylesheet" type="text/css"/>
+	<link href="css/main.css" rel="stylesheet" type="text/css"/>
 	<title>Dream team blog</title>
     </head>
     <body>
@@ -18,6 +19,7 @@
 		</div>
 		<div class="content">	
 		    <div id="menu">
+			
 			<a href="<?= url('index'); ?>">Все новости</a> <a href="<?= url('all'); ?>">Мои новости</a> <a href="index.php?action=exit">Выйти</a>
 		    </div>		    
 		    <div class="inner_copy"></div>
