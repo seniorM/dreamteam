5992da9e59b7b7704802642a470ad54d1fce7c1e
totoseriapovlup <totoseriapov@yandex.ua>
2019-09-09 19:55:07 +0300

    format

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -1,10 +1,11 @@
 <?php
- session_start();
-include_once 'beer'.DIRECTORY_SEPARATOR.'index.php';
-include_once 'english'.DIRECTORY_SEPARATOR.'index.php';
-//include_once 'georgia'.DIRECTORY_SEPARATOR.'index.php';
-include_once 'later'.DIRECTORY_SEPARATOR.'index.php';
-include_once 'cold'.DIRECTORY_SEPARATOR.'index.php';
-include_once 'toto'.DIRECTORY_SEPARATOR.'index.php';
+
+session_start();
+include_once 'beer' . DIRECTORY_SEPARATOR . 'index.php';
+include_once 'english' . DIRECTORY_SEPARATOR . 'index.php';
+include_once 'georgia'.DIRECTORY_SEPARATOR.'index.php';
+include_once 'later' . DIRECTORY_SEPARATOR . 'index.php';
+include_once 'cold' . DIRECTORY_SEPARATOR . 'index.php';
+include_once 'toto' . DIRECTORY_SEPARATOR . 'index.php';
 
 
