53554ba292fb5cdf2e1476bb0d0238bf8e119a0d
totoseriapovlup <totoseriapov@yandex.ua>
2019-09-09 19:56:57 +0300

    format

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
-include_once 'includes'.DIRECTORY_SEPARATOR.'config.php';
-include_once 'includes'.DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'functions.php';
+
+include_once 'includes' . DIRECTORY_SEPARATOR . 'config.php';
+include_once 'includes' . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR . 'functions.php';
 router();
