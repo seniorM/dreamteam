10a745512c85f0fd1c56b5723ee0568710fd33f9
kazaday <Kazadaymaksim@gmail.com>
2019-09-09 18:50:57 +0300

    fix all.php

--- includes/views/pages/all.php
+++ includes/views/pages/all.php
@@ -1,21 +1,17 @@
 <div id="all">
-        <form action="index.php?action=add" method="post" name="add">
-            <label>Heading: <input type="text" name="heading"></label>
-            <label>Text<textarea name="text"></textarea></label>
-            <input type="submit" value="add post">
-        </form>
-        <?php if (empty(get_posts(get_auth_user()))): ?>
-            <h2>создай свой первый пост </h2>
-        <?php else: ?>
-            <?php foreach (get_posts(get_auth_user()) as $id => $post): ?>
-                <div class="<?= $post['login']; ?>">
-                    <h2><?= $post['heading']; ?></h2>
-                    <?= $post['text']; ?>
-                    <form action="index.php?action=delete" method="post" name="delete">
-                        <input type="text" value="<?= $id; ?>" hidden name="id">
-                        <input type="submit" value="delete">
-                    </form>
-                </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
+    <form action="index.php?action=add" method="post" name="add">
+        <label>Heading: <input type="text" name="heading"></label>
+        <label>Text<textarea name="text"></textarea></label>
+        <input type="submit" value="add post">
+    </form>
+    <?php foreach (get_posts(get_auth_user()) as $id => $post): ?>
+        <div class="<?= $post['login']; ?>">
+            <h2><?= $post['heading']; ?></h2>
+            <?= $post['text']; ?>
+            <form action="index.php?action=delete" method="post" name="delete">
+                <input type="text" value="<?= $id; ?>" hidden name="id">
+                <input type="submit" value="delete">
+            </form>
+        </div>
+    <?php endforeach; ?>
 </div>
