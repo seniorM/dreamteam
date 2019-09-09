2e5963d09aa48b5461b3bea27e115f780ff8a2b2
Vasyl <admin@gmail.com>
2019-09-09 20:27:07 +0300

    changes to includes/views/pages/index.php

--- includes/views/pages/index.php
+++ includes/views/pages/index.php
@@ -2,11 +2,11 @@
 <div id="all_users_posts">
     <?php $posts = get_posts(); ?>
     <?php foreach ($posts as $id => $post) : ?>
-    <div class="post">
-	<h2><?= $post['heading'] ?></h2>
-	<div><?= $post['text']?></div>
-	<div><?= $post['login']?></div>
-	<div><?= $post['date']?></div>
-    </div>
-    <?php endforeach;?>
+        <div class="post">
+    	<h2><?= $post['heading'] ?></h2>
+    	<div><?= $post['text'] ?></div>
+    	<div><label>автор: <?= $post['login'] ?></label></div>
+    	<div><?= $post['date'] ?></div>
+        </div>
+    <?php endforeach; ?>
 </div>
