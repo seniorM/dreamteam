1cf8d24778740e9ab75711bd78828f42b856a9cc
totoseriapovlup <totoseriapov@yandex.ua>
2019-09-09 18:29:46 +0300

    change all posts

--- includes/functions/toto/index.php
+++ includes/functions/toto/index.php
@@ -4,6 +4,13 @@ function url($action) {
     return 'index.php?action=' . $action;
 }
 
+function redirect($url){
+    header('Location:' . $url);
+}
+
 function get_index(){
+    if(!is_auth()){
+	redirect(url('login'));
+    }
     show('index.php');
 }
--- includes/views/pages/index.php
+++ includes/views/pages/index.php
@@ -1,18 +1,10 @@
 <a href="<?= url('all') ?>">Мои записи</a>
 <div id="all_users_posts">
-    <?php
-    if ((is_auth())) {
-	$posts = get_posts();
-	if (isset($posts)) {
-	    foreach ($posts as $id => $post) {
-		echo '<h2>' . $post['heading'] . '</h2>';
-		echo $post['text'];
-	    }
-	} else {
-	    echo 'здесь будут все посты';
-	}
-    } else {
-	header('Location:' . url('login'));
-    }
-    ?>
+    <?php $posts = get_posts(); ?>
+    <?php foreach ($posts as $id => $post) : ?>
+    <div class="post">
+	<h2><?= $post['heading'] ?></h2>
+	<div>$post['text']</div>
+    </div>
+    <?php endforeach;?>
 </div>
