aeb45d0c116e2fb9c3eb9223bb1609cabfbef9e8
Vasyl <admin@gmail.com>
2019-09-09 20:44:49 +0300

    formatting

--- includes/views/pages/index.php
+++ includes/views/pages/index.php
@@ -1,11 +1,12 @@
 <div id="all_users_posts">
     <?php $posts = get_posts(); ?>
-    <?php foreach ($posts as $id => $post) : ?>
+    <?php foreach ($posts as $post) : ?>
         <div class="post">
-    	<h2><?= $post['heading'] ?></h2>
-    	<div><?= $post['text'] ?></div>
-    	<div><label>автор: <?= $post['login'] ?></label></div>
-    	<div><?= $post['date'] ?></div>
+	    <h2><?= $post['heading'] ?></h2>
+	    <div><?= $post['text'] ?></div>
+	    <div><label>автор: <?= $post['login'] ?></label></div>
+	    <div><?= $post['date'] ?></div>
         </div>
     <?php endforeach; ?>
 </div>
+А
