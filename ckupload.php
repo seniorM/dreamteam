014e59b3319870e5111806e02cff46ab04fcb8f6
totoseriapovlup <totoseriapov@yandex.ua>
2019-09-13 18:58:15 +0300

    ckEditor upload: "It's alive"

--- ckupload.php
+++ ckupload.php
@@ -1,17 +1,22 @@
 <?php
 
-$callback = $_GET['CKEditorFuncNum'];
-    $file_name = $_FILES['upload']['name'];
-    $file_name_tmp = $_FILES['upload']['tmp_name'];
-    $file_new_name = $_SERVER['DOCUMENT_ROOT'].'/upload/';
-    $full_path = $file_new_name.$file_name;
-    $http_path = '/upload/'.$file_name;
-    $error = '';
-    if( move_uploaded_file($file_name_tmp, $full_path) ) {
-    } else {
-     $error = 'Some error occured please try again later';
-     $http_path = '';
-    }
-    echo "";
+$file_name = $_FILES['upload']['name'];
+$file_name_tmp = $_FILES['upload']['tmp_name'];
+$file_new_name = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'upload';
+$full_path = $file_new_name . DIRECTORY_SEPARATOR . $file_name;
+$http_path = '/upload/' . $file_name;
+$error = '';
+header('Content-type: application/json; charset=utf-8'); 
+if (move_uploaded_file($file_name_tmp, $full_path)) {
+    echo '{
+        "uploaded": true,
+        "url": "'.$http_path.'"}';
+} else {
+    echo '{
+        "uploaded": false,
+        "error": {
+            "message": "could not upload this image1"
+        }';
+}
 
 
